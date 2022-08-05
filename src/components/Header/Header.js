8228de42a1e720047c6629843d46beb5846fce16
Svetlana A. Pivovarova <pivovarova.svetlana@gmail.com>
2022-08-05 07:51:26 +0400

    сверстать header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import logoHeader from '../../images/logo.svg'; // Путь к изображению внутри сборки
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return(
+        <header className="header page__content">
+            <img className="header__logo" src={logoHeader} alt="Логотип сайта"/>
+            <ul className="header__navigation">
+                <li className="header__navigation-item">Регистрация</li>
+                <li className="header__navigation-item">
+                    <button className="header__auth-button">Войти</button>
+                </li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Main() {
+
+    return(
+        <main className="content">
+            <section className="profile content__section">
+
+            </section>
+            <section className="elements content__section">
+
+            </section>
+        </main>
+    )
+}
+
+export default Main;
